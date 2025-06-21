62b471da24c485c64b53c5a7444ede4dd241dea6
lan1177 <hezilan2000@outlook.com>
2025-06-21 23:27:30 +0800

    feat: Add vercel config and gitignore

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ load_dotenv()
 
 class Config:
     # 飞书应用配置
-    FEISHU_APP_ID = os.getenv("FEISHU_APP_ID", "cli_a7716f1ed63cd01c")
-    FEISHU_APP_SECRET = os.getenv("FEISHU_APP_SECRET", "JGjdVIbF6On96l8mJBM72cevqTmVQHtE")
+    FEISHU_APP_ID = os.getenv("FEISHU_APP_ID")
+    FEISHU_APP_SECRET = os.getenv("FEISHU_APP_SECRET")
     
     # 多维表格配置
-    BASE_ID = os.getenv("BASE_ID", "Neqabr2EIaANJEsjg2DcK0Osn8g")
-    TABLE_ID = os.getenv("TABLE_ID", "tblEqTy7bRR4Hdau")
+    BASE_ID = os.getenv("BASE_ID")
+    TABLE_ID = os.getenv("TABLE_ID")
     
     # Flask配置
     SECRET_KEY = os.urandom(24)
